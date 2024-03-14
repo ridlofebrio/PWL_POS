6ef320546a7a372d284193e11d4ee089f7d479ec
Febrio <muhridlo111@gmail.com>
2024-03-14 16:28:31 +0700

    Praktikum 2.5 – Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,15 +56,21 @@ class UserController extends Controller
 
         // $user = UserModel::where('level_id', 2)->count();
 
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager tiga tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id'=> 2
             ],
         );
+        $user -> username='manager12';
         $user -> save();
-        return view('user', ['data' => $user]);
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username','level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama','username']));
+
     }
 }
