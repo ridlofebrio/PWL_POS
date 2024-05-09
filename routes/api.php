e541195907e7713498c1519c4119e1d43426e1df
Febrio <muhridlo111@gmail.com>
2024-05-09 09:20:12 +0700

    Praktikum 4

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,6 +22,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/register', RegisterController::class)->name('register');
-
 Route::post('/logout', LogoutController::class)->name('logout');
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
